0c6fade7c3fe02fbe5ffaf666ef1c4e8438e012d
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-11-21 23:23:57 +0900

    写真登録でバリデーションを追加してエラーメッセージを表示する

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,7 @@
 class Photo < ApplicationRecord
   has_one_attached :image_file
   belongs_to :user
+  validates :title, presence: true
+  validates :title, length: { maximum: 30 }
+  validates :image_file, presence: true
 end
