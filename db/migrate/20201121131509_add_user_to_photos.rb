1c518123f3015119cc6f2a714bd0714faa7be622
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-11-21 22:20:58 +0900

    userの外部キーにはuuidを使用する

--- db/migrate/20201121131509_add_user_to_photos.rb
+++ db/migrate/20201121131509_add_user_to_photos.rb
@@ -1,5 +1,5 @@
 class AddUserToPhotos < ActiveRecord::Migration[6.0]
   def change
-    add_reference :photos, :user, null: false, foreign_key: true
+    add_reference :photos, :user, type: :uuid, null: false, foreign_key: true
   end
 end
