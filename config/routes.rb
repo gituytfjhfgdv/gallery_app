a7f201ed3d99307535542b0eee199b28c917dc72
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-11-21 23:09:19 +0900

    画像投稿したら、画像投稿一覧に表示される

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :photos, only: %i[index]
+  resources :photos, only: %i[index new create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'login',  to: 'sessions#new'
   post 'login', to: 'sessions#create'
