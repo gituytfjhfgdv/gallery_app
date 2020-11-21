4f205d8d4c7cd1f87a088e5604551437d869940a
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-11-21 16:24:30 +0900

    ログイン機能のroutingを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'sessions/new'
-  get 'sessions/create'
+  get 'login',  to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 end
