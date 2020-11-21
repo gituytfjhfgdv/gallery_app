3cce0c3d1c68c3cc3c052f7b57533e4e0e1f1ae3
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-11-21 19:06:05 +0900

    存在するユーザでログインができるようにする

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,43 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
+  before_action :user, only: :create
+  before_action :permit_only_valid_user, only: :create
+
   def new
+    @sign_in_params ||= { email: '', password: '' }
+    @errors ||= []
   end
 
   def create
+    login(@user)
+    redirect_to login_path, notice: '成功！'
+  end
+
+  private
+
+  def sign_in_params
+    params.require(:session).permit(:email, :password)
+  end
+
+  def permit_only_valid_user
+    @sign_in_params = sign_in_params
+    errors_messages
+    unless @user&.authenticate(@sign_in_params[:password]) && @errors.blank?
+      render :new
+      return
+    end
+  end
+
+  def errors_messages
+    @errors = []
+    @errors << 'ユーザidを入力してください' if @sign_in_params[:email].blank?
+    @errors << 'パスワードを入力してください' if @sign_in_params[:password].blank?
+    return unless @user.blank? && @sign_in_params[:email].present? && @sign_in_params[:password].present?
+
+    @errors << '入力された情報で登録されたユーザが見つかりませんでした。'
+  end
+
+  def user
+    @user = User.find_by_email(sign_in_params[:email])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true,
+                    format: /\A[-a-z0-9_+.]+@([-a-z0-9]+\.)+[a-z0-9]{2,}\z/i
+  validates :email, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
