95711e058743fd7000e605dd7d9c2f33be069784
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-11-21 21:12:42 +0900

    photoのtitleはnullを許可しない

--- db/migrate/20201121121145_create_photos.rb
+++ db/migrate/20201121121145_create_photos.rb
@@ -1,7 +1,7 @@
 class CreatePhotos < ActiveRecord::Migration[6.0]
   def change
     create_table :photos do |t|
-      t.string :title
+      t.string :title, null: false
 
       t.timestamps
     end
