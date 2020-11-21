e5f4732310c8b270c083dd5bc4adfc95a796207c
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-11-21 22:14:50 +0900

    写真一覧ページでログインしてるユーザの登録した写真を表示する

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
   include UserAuthentication
   def index
+    @photos = current_user.photos.order(created_at: :desc)
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,8 @@ module SessionsHelper
   def login(user)
     session[:user_id] = user.id
   end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
