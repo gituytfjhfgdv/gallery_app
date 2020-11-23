77e310688c3229cd8968d4cfab731d0f1c4b14e9
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-11-23 16:02:43 +0900

    rootパスはログインページへ飛ぶ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :photos, only: %i[index new create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'sessions#new'
   get 'login',  to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
+
+  resources :photos, only: %i[index new create]
 
   get '/oauth/callback', to: 'o_authes#create'
   post 'tweet/:photo_id', to: 'o_authes#tweet', as: :new_tweet
