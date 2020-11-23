5ebe164b0966242417e08087b50f0758c50d7470
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-11-23 17:13:00 +0900

    環境構築用で開発をしやすくするためのseedを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = User.create([{ email: 'user1@tech.com', password: 'first_user_pass' },
+                     { email: 'user2@tech.com', password: 'second_user_pass' }])
+file_path = Rails.root.join('test', 'fixtures', 'images', 'dummy_photo.jpg')
+photo = Photo.new(title: 'first photo', user: users.first)
+photo.image_file.attach(io: File.open(file_path), filename: 'dummy_photo.jpg')
+photo.save
